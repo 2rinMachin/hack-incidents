f9fee60f5768d7de48abaf50249b551480c73012
dfigueroaw <dfigueroaw@gmail.com>
2025-11-16 18:45:06 -0500

    feat: add pagination to incidents endpoint

--- functions/get_incidents.py
+++ functions/get_incidents.py
@@ -1,6 +1,6 @@
 import boto3
 from boto3.dynamodb.conditions import Attr
-
+import json
 from common import response
 
 dynamodb = boto3.resource("dynamodb")
@@ -15,39 +15,41 @@ def handler(event, context):
     urgency = query.get("urgency")
     location = query.get("location")
 
+    limit = int(query.get("limit", 20))
+    next_token = query.get("next_token")
+
     filter_expression = None
 
     if kind:
         expr = Attr("kind").eq(kind)
-        filter_expression = (
-            expr if filter_expression is None else filter_expression & expr
-        )
+        filter_expression = expr if filter_expression is None else filter_expression & expr
 
     if status:
         expr = Attr("status").eq(status)
-        filter_expression = (
-            expr if filter_expression is None else filter_expression & expr
-        )
+        filter_expression = expr if filter_expression is None else filter_expression & expr
 
     if urgency:
         expr = Attr("urgency").eq(urgency)
-        filter_expression = (
-            expr if filter_expression is None else filter_expression & expr
-        )
+        filter_expression = expr if filter_expression is None else filter_expression & expr
 
     if location:
-        # For partial matches, use contains
         expr = Attr("location").contains(location)
-        filter_expression = (
-            expr if filter_expression is None else filter_expression & expr
-        )
+        filter_expression = expr if filter_expression is None else filter_expression & expr
+
+    scan_kwargs = {"Limit": limit}
 
     if filter_expression:
-        resp = incidents.scan(FilterExpression=filter_expression)
-    else:
-        resp = incidents.scan()
+        scan_kwargs["FilterExpression"] = filter_expression
 
-    items: list[dict] = resp.get("Items", [])
+    if next_token:
+        scan_kwargs["ExclusiveStartKey"] = json.loads(next_token)
+
+    resp = incidents.scan(**scan_kwargs)
+
+    items = resp.get("Items", [])
     sorted_items = sorted(items, key=lambda x: x.get("created_at", ""), reverse=True)
 
-    return response(200, sorted_items)
+    return response(200, {
+        "items": sorted_items,
+        "next_token": json.dumps(resp["LastEvaluatedKey"]) if "LastEvaluatedKey" in resp else None
+    })
