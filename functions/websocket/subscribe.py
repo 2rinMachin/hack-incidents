011994bf98dba81df6a197f8e1156a1bbf9d06ce
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-16 01:27:35 -0500

    fix: maybe fix subscriptions

--- functions/websocket/subscribe.py
+++ functions/websocket/subscribe.py
@@ -1,34 +1,21 @@
 import boto3
 from boto3.dynamodb.conditions import Key
-from pydantic import BaseModel
 
-from common import parse_body, response
+from common import response
 from schemas import IncidentSubscription
 
 dynamodb = boto3.resource("dynamodb")
-subscriptions = dynamodb.Table("hack-incidents-subscriptions")
-
-
-class SubscribeRequest(BaseModel):
-    connection_id: str
+subscriptions = dynamodb.Table("hack-incident-subscriptions")
 
 
 def handler(event, context):
-    data, err = parse_body(SubscribeRequest, event)
-    if err != None:
-        return err
-
-    assert data != None
-
     connection_id = event["requestContext"]["connectionId"]
 
-    resp = subscriptions.query(
-        KeyConditionExpression=Key("connection_id").eq(connection_id),
-        Limit=1,
-    )
-
-    if resp.get("Count") > 0:
+    try:
+        subscriptions.get_item(Key={"connection_id": connection_id})
         return response(200, {"message": "Already subscribed."})
+    except KeyError:
+        pass
 
     new_subscription = IncidentSubscription(connection_id=connection_id)
     subscriptions.put_item(Item=new_subscription.model_dump())
